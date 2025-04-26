4653d7fe346546d80e0e4b0a176bab2338ead06a
krosslerfrancisco <krosslerfrancisco@gmail.com>
2025-04-26 21:53:55 +0000

    post

--- api/get-pedidos.js
+++ api/get-pedidos.js
@@ -6,13 +6,13 @@ export default async function handler(req, res) {
     await connectToDatabase();
 
     const SaleSchema = new mongoose.Schema({}, { strict: false });
-    const Sale = mongoose.models.Sale || mongoose.model('Sale', SaleSchema, 'gangain');
+    const Sale = mongoose.models.Sale || mongoose.model('Sale', SaleSchema, 'Ventas'); // <- corregido
 
     const sales = await Sale.find({});
 
     res.status(200).json(sales);
   } catch (error) {
-    console.error('Erro ao buscar vendas:', error);
-    res.status(500).json({ message: 'Erro ao buscar vendas', error: error.message });
+    console.error('Error al buscar ventas:', error);
+    res.status(500).json({ message: 'Error al buscar ventas', error: error.message });
   }
 }
