c461c026845a16e0ff643388ac12cdda5186c475
krosslerfrancisco <krosslerfrancisco@gmail.com>
2025-04-27 01:08:08 +0000

    aaaaa

--- src/pages/components/DashboardMain.jsx
+++ src/pages/components/DashboardMain.jsx
@@ -6,10 +6,10 @@ function DashboardMain({user}) {
   return (
     <div className="main-container">
         <h2>Hola {user.username.charAt(0).toUpperCase() + user.username.slice(1)}, Bienvenido</h2>
-        <p>Desde el escritorio de tu cuenta puedes ver tu historial de compras, productos favoritos y detalles de tu cuenta.</p>
+        <p>Desde el panel de tu cuenta puedes ver tu historial de compras, productos favoritos y detalles de tu cuenta.</p>
         <div className="main-links-container">
             <Link to={"./orders"}>Pedidos</Link>
-            <Link to={"./details"}>Mis Datos</Link>
+            <Link to={"./details"}>Mis datos</Link>
             <Link to={"./favorites"}>Favoritos</Link>
         </div>
     </div>
